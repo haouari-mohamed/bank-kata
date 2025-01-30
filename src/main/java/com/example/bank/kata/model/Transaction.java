e6a6070769c7d3888bb889d0072f278f549d2e52
haouari-mohamed <haouari-mohamed>
2025-01-30 19:18:39 +0100

    create the class Transaction

--- src/main/java/com/example/bank/kata/model/Transaction.java
+++ src/main/java/com/example/bank/kata/model/Transaction.java
@@ -1,4 +1,32 @@
 package com.example.bank.kata.model;
 
+import java.time.LocalDate;
+
 public class Transaction {
+    private LocalDate date;
+    private int amount;
+    private int balance;
+
+    public Transaction(LocalDate date, int amount, int balance) {
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return date + " | " + amount + " | " + balance;
+    }
 }
